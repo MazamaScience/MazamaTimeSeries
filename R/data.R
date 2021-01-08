e57b44988339717ba5eee6eb8ffdda0646f6e8c7
Jonathan Callahan <jonathan.s.callahan@gmail.com>
2021-01-08 12:37:01 -0800

    feat: fleshing out sts utility functions

--- R/data.R
+++ R/data.R
@@ -1,7 +1,7 @@
-#' @title Example PurpleAir Timeseries dataset
+#' @title Example SingleTimeSeries dataset
 #' @format An S3 object composed of "meta" and "data" dataframes.
-#' @description The \code{example_sts} dataset provides a quickly loadable version of
-#' an \emph{sts} object for practicing and code examples.
+#' @description The \code{example_sts} dataset provides a quickly loadable
+#' version of an \emph{sts} object for practicing and code examples.
 #'
 #' This dataset was was generated on 2021-01-08 by running:
 #'
--- R/utils-sts.R
+++ R/utils-sts.R
@@ -1,6 +1,6 @@
 #' @export
 #'
-#' @name sts_isSts
+#' @name sts_isValid
 #' @title Test \emph{sts_timeseries} object for correct structure
 #'
 #' @param sts \emph{sts_timeseries} object
@@ -35,9 +35,9 @@
 #' @examples
 #' library(MazamaTimeSeries)
 #'
-#' sts_isSts(example_sts)
+#' sts_isValid(example_sts)
 #'
-sts_isSts <- function(
+sts_isValid <- function(
   sts = NULL
 ) {
 
@@ -90,12 +90,137 @@ sts_isSts <- function(
 #' sts_isEmpty(example_sts)
 #'
 sts_isEmpty <- function(sts) {
+
   MazamaCoreUtils::stopIfNull(sts)
-  # NOTE:  Use minimal validation
+  # NOTE:  Use minimal validation for improved speed
   if ( !'data' %in% names(sts) || !'data.frame' %in% class(sts$data) )
     stop("Not a valid 'sts' object.")
 
   return( nrow(sts$data) == 0 )
+
 }
 
+
+#' @importFrom rlang .data
+#' @export
+#'
+#' @title Retain only distinct data records in sts$data
+#'
+#' @param sts \emph{sts} object
+#'
+#' @return A \emph{sts} object with no duplicated data records.
+#'
+#' @description Three successive steps are used to guarantee that the
+#' \code{datetime} axis contains no repeated values:
+#'
+#' \enumerate{
+#' \item{remove any duplicate records}
+#' \item{guarantee that rows are in \code{datetime} order}
+#' \item{average together fields for any remaining records that share the same
+#' \code{datetime}}
+#' }
+#'
+sts_distinct <- function(sts) {
+
+  # NOTE:  Use minimal validation for improved speed
+  if ( !'data' %in% names(sts) || !'data.frame' %in% class(sts$data) )
+    stop("Not a valid 'sts' object.")
+
+  sts$data <-
+    sts$data %>%
+    dplyr::distinct() %>%
+    dplyr::arrange(.data$datetime) %>%
+    .replaceRecordsWithDuplicateTimestamps()
+
+  return( sts )
+
+}
+
+
+#' @title Extract dataframes from \emph{sts} objects
+#'
+#' @description
+#' These functions are convenient wrappers for extracting the dataframes that
+#' comprise a \emph{sts} object. These functions are designed to be useful when
+#' manipulating data in a pipeline chain using \code{\%>\%}.
+#'
+#' Below is a table showing equivalent operations for each function.
+#'
+#' \tabular{ll}{
+#'   \strong{Function} \tab \strong{Equivalent Operation}\cr
+#'   \code{sts_extractData(sts)} \tab \code{sts$data}\cr
+#'   \code{sts_extractMeta(sts)} \tab \code{sts$meta}
+#' }
+#'
+#' @param sts \emph{sts} object to extract dataframe from.
+#'
+#' @return A dataframe from the given \emph{sts} object
+#'
+#' @name sts_extractDataFrame
+#' @aliases sts_extractData sts_extractMeta
+#'
+NULL
+
+
+#' @export
+#' @rdname sts_extractDataFrame
+#'
+sts_extractData <- function(sts) {
+
+  # NOTE:  Use minimal validation for improved speed
+  if ( !'data' %in% names(sts) || !'data.frame' %in% class(sts$data) )
+    stop("Not a valid 'sts' object.")
+
+  return(sts$data)
+
+}
+
+
+#' @export
+#' @rdname sts_extractDataFrame
+#'
+sts_extractMeta <- function(sts) {
+
+  # NOTE:  Use minimal validation for improved speed
+  if ( !'meta' %in% names(sts) || !'data.frame' %in% class(sts$meta) )
+    stop("Not a valid 'sts' object.")
+
+  return(sts$meta)
+
+}
+
+
+# ===== INTERNAL FUNCTIONS =====================================================
+
+.replaceRecordsWithDuplicateTimestamps <- function(df) {
+
+  # NOTE:  Some time series datasets can have multiple records with the same
+  # NOTE:  'datetime'. This might occur when times are forced to the nearest
+  # NOTE:  second, minute or hour. This assumes that the incoming df is
+  # NOTE:  arranged by datetime and averages together all records that share the
+  # NOTE:  same 'datetime'.
+  if ( any(duplicated(df$datetime)) ) {
+
+    # Find duplicate records
+    duplicateIndices <- which(duplicated(df$datetime))
+    for ( index in duplicateIndices ) {
+
+      # Record immediately prior will be the other record with this timestamp
+      replacementRecord <-
+        dplyr::slice(df, (index-1):index) %>%
+        dplyr::summarise_all(mean, na.rm = TRUE)
+
+      # Replace the original record with the mean record
+      df[(index-1),] <- replacementRecord
+
+    }
+
+    # Kep all the non-duplicate timestamp records
+    df <- df[!duplicated(df$datetime),]
+
+  }
+
+  return(df)
+
+}
 
